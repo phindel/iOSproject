bab88f3288850b851c0828c33f91c147d1e2d8c5
tp <tp@tps-macbook-pro.local>
2016-03-04 16:10:22 +0100

    Initial Commit

---
+++ HelpProject/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HelpProject
+//
+//  Created by tp on 04/03/2016.
+//  Copyright © 2016 del_leo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
