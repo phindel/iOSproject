38b95e6397660fdc20faec014742d576132070e2
phindel <phindel@users.noreply.github.com>
2016-03-04 16:32:17 +0100

    Update ViewController.swift

--- HelpProject/ViewController.swift
+++ HelpProject/ViewController.swift
@@ -2,7 +2,7 @@
 //  ViewController.swift
 //  HelpProject
 //
-//  Created by tp on 04/03/2016.
+//  Created by delphin_leopold on 04/03/2016.
 //  Copyright © 2016 del_leo. All rights reserved.
 //
 
@@ -12,12 +12,10 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
 
