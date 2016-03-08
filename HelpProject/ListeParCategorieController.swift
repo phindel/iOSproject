7d6b98c6345838d12fa650ddc9bb553aa989d9ac
tp <tp@tps-macbook-pro.local>
2016-03-08 19:50:04 +0100

    Sélection par catégorie

--- HelpProject/ListeParCategorieController.swift
+++ HelpProject/ListeParCategorieController.swift
@@ -1,5 +1,5 @@
 //
-//  ListeCategorieTableViewController.swift
+//  ListeParCategorieController.swift
 //  HelpProject
 //
 //  Created by tp on 08/03/2016.
@@ -7,92 +7,53 @@
 //
 
 import UIKit
-import CoreData
 
-class ListeCategorieTableViewController: UITableViewController {
+class ListeParCategorieController: UITableViewController {
 
-    var table1=["Ayy","fdg","V"]
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+
         // Uncomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
-        
+
         // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
         // self.navigationItem.rightBarButtonItem = self.editButtonItem()
     }
-    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
-        //return the number of sections
-        return 1
-    }
-    
-    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        //return the number of rows
-        return 3
-    }
-    func rafraichir(){
-        //table1[1]+="j"
-        let appDel:AppDelegate=UIApplication.sharedApplication().delegate as! AppDelegate
-        let contexte:NSManagedObjectContext=appDel.managedObjectContext
-        let req=NSFetchRequest(entityName: "Personne")
-        req.returnsObjectsAsFaults=false
-        
-        do{
-            let res=try contexte.executeFetchRequest(req)
-            if res.count>0{
-                for r in res as! [NSManagedObject]{
-                    print(r.valueForKey("nom")!)
-                    
-                }
-            }
-        }catch{
-            print("Echec du fetch!")
-        }
-    }
-    
-    
-    
-    @IBAction func refresh(sender: UIRefreshControl) {
-        refreshControl?.attributedTitle=NSAttributedString(string:"yo man")
-        //rafraichir()
-        table1[1]+="j"
-        self.tableView.reloadData()
-        refreshControl?.endRefreshing()
-    }
-    func refresh2(sender: UIRefreshControl) {
-        
-    }
-    
-    var t=0
-    
-    
-    
-    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        //let cell = tableView.dequeueReusableCellWithIdentifier("typeGen", forIndexPath: indexPath)
-        let cell=UITableViewCell(style: .Value1, reuseIdentifier: "typeGen")
-        cell.textLabel!.text=table1[indexPath.item]
-        cell.detailTextLabel!.text=table1[indexPath.item]+" lol"
-        cell.imageView!.image=UIImage(named: "rond.png")
-        
-        return cell
-    }
-   
+
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
-        performSegueWithIdentifier("segueListeParCat", sender: indexPath.row)
+    var categorie=""
+    // MARK: - Table view data source
+
+    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 1
     }
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        if(segue.identifier=="segueListeParCat"){
-            if let indice = tableView.indexPathsForSelectedRows{
-                let dvc=segue.destinationViewController as! ListeParCategorieController
-                dvc.categorie=table1[(indice.first?.item)!]
-            }
-        }
+
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return 1
     }
-    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        //let cell = tableView.dequeueReusableCellWithIdentifier("typeGen", forIndexPath: indexPath)
+        let cell=UITableViewCell(style: .Value1, reuseIdentifier: "typeDemande")
+        cell.textLabel!.text=categorie
+        cell.detailTextLabel!.text="TODO"
+        cell.imageView!.image=UIImage(named: "rond.png")
+        
+        return cell
+    }
+    /*
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("reuseIdentifier", forIndexPath: indexPath)
+
+        // Configure the cell...
+
+        return cell
+    }
+    */
 
     /*
     // Override to support conditional editing of the table view.
