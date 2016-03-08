75581c3d9e3edef83cb4dec8ffa626aba7c8ed24
tp <tp@tps-macbook-pro.local>
2016-03-08 19:00:35 +0100

    Fenêtres vides

---
+++ HelpProject/ListeCategorieTableViewController.swift
@@ -0,0 +1,135 @@
+//
+//  ListeCategorieTableViewController.swift
+//  HelpProject
+//
+//  Created by tp on 08/03/2016.
+//  Copyright © 2016 del_leo. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+class ListeCategorieTableViewController: UITableViewController {
+
+    var table1=["Ayy","fdg","V"]
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+        
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
+    }
+    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        //return the number of sections
+        return 1
+    }
+    
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        //return the number of rows
+        return 3
+    }
+    func rafraichir(){
+        //table1[1]+="j"
+        let appDel:AppDelegate=UIApplication.sharedApplication().delegate as! AppDelegate
+        let contexte:NSManagedObjectContext=appDel.managedObjectContext
+        let req=NSFetchRequest(entityName: "Personne")
+        req.returnsObjectsAsFaults=false
+        
+        do{
+            let res=try contexte.executeFetchRequest(req)
+            if res.count>0{
+                for r in res as! [NSManagedObject]{
+                    print(r.valueForKey("nom")!)
+                    
+                }
+            }
+        }catch{
+            print("Echec du fetch!")
+        }
+    }
+    
+    
+    var t=0
+    @IBAction func refresh(sender: UIRefreshControl) {
+        refreshControl?.attributedTitle=NSAttributedString(string:"yo man")
+        rafraichir()
+        self.tableView.reloadData()
+        refreshControl?.endRefreshing()
+    }
+    
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        //let cell = tableView.dequeueReusableCellWithIdentifier("typeGen", forIndexPath: indexPath)
+        let cell=UITableViewCell(style: .Value1, reuseIdentifier: "typeGen")
+        cell.textLabel!.text=table1[indexPath.item]
+        cell.detailTextLabel!.text=table1[indexPath.item]+" lol"
+        cell.imageView!.image=UIImage(named: "rond.png")
+        
+        return cell
+    }
+   
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        performSegueWithIdentifier("segue2", sender: indexPath.row)
+    }
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        if(segue.identifier=="segue2"){
+            if let indice = tableView.indexPathsForSelectedRows{
+                /*let dvc=segue.destinationViewController as! DetailViewController
+                dvc.setDetail(table1[(indice.first?.item)!])*/
+            }
+        }
+    }
+    
+
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+    */
+
+    /*
+    // Override to support editing the table view.
+    override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
+        if editingStyle == .Delete {
+            // Delete the row from the data source
+            tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
+        } else if editingStyle == .Insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+    */
+
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(tableView: UITableView, moveRowAtIndexPath fromIndexPath: NSIndexPath, toIndexPath: NSIndexPath) {
+
+    }
+    */
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(tableView: UITableView, canMoveRowAtIndexPath indexPath: NSIndexPath) -> Bool {
+        // Return false if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
